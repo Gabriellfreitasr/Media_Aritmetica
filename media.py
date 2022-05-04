c38372bab3660b5e1d5c8b814f453520adc4cb66
Gabriellfreitasr <GabriellFreitas456@gmail.com>
2022-05-04 18:48:51 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,9 @@
+#Media aritmética
+print("\t ----Media Aritmética----")
+
+nota1 = int(input("Digite o valor da primeira nota: "))
+nota2 = int(input("Digite o valor da segunda nota: "))
+
+total = (nota1 + nota2)/2
+
+print ("A media aritmética é de: {}".format(total))
